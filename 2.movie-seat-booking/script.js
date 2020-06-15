93fb4c73fb74813b1691e1155feef1fc8ef8679b
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-06-15 11:08:18 +0300

    save movie seats price and selected option in local storage

--- 2.movie-seat-booking/script.js
+++ 2.movie-seat-booking/script.js
@@ -5,15 +5,32 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
+
+//get data from local storage and update ui
+populateUI();
+
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  //save to local storage
+  //copy selected seats into arr map through it and retun new array of indexes from seats array
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  console.log(seatsIndex);
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 };
+
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("movieIndex", movieIndex);
+  localStorage.setItem("moviePrice", moviePrice);
+};
 //movie select event
 movieSelect.addEventListener("change", (e) => {
-  // console.log(e.target.value);
+  // console.dir(e.target);
+  //get index and value of selected
+  setMovieData(e.target.selectedIndex, e.target.value);
   ticketPrice = +e.target.value;
   updateSelectedCount();
 });
@@ -27,8 +44,26 @@ container.addEventListener("click", (e) => {
     //length value
     //add(token) remove(token) toggle(token)
     //item(index) contains(token) entries() keys() values() forEach()
+
     e.target.classList.toggle("selected");
   }
   updateSelectedCount();
   // console.log(e.target.classList);
 });
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  const selectedMovieIndex = localStorage.getItem("movieIndex");
+  const selectedMoviePrice = localStorage.getItem("moviePrice");
+  if (selectedSeats && selectedSeats.length > 0) {
+    selectedSeats.forEach((ind) => seats[ind].classList.add("selected"));
+  }
+  if (selectedMovieIndex) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+  if (selectedMoviePrice) {
+    ticketPrice = selectedMoviePrice;
+  }
+}
+
+updateSelectedCount();
